35191f0e30366bfdaee9b36d776715ab1b42b185
Swair Shah <swairshah@gmail.com>
2020-04-19 13:57:37 -0700

    publishconf similar to pelicanconf

--- publishconf.py
+++ publishconf.py
@@ -12,12 +12,24 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://swairshah.github.io'
-RELATIVE_URLS = False
+#RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
+THEME = 'TuftePelican'
+PLUGIN_PATHS = ['/Users/swairshah/work/writing/pelican-plugins']
+PLUGINS = ['render_math', 'code_include','md_inline_extension'] #'simple_footnotes']
 DELETE_OUTPUT_DIRECTORY = True
+SUMMARY_MAX_LENGTH=0
+
+MD_INLINE = {
+    #'[+]': 'sidenote-number',
+    #'[-]': 'sidenote',
+    '[+]': 'dt-note',
+    '=+=': 'framed'
+    #'|-|': ('color:blue;', 'sidenote'),
+}
 
 # Following items are often useful when publishing
 
